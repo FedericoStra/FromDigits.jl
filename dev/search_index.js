b1acacc47377eda7c3637da23e5e555b51f2b53e
Documenter.jl <documenter@juliadocs.github.io>
2021-04-13 13:16:26 +0000

    build based on 5bb97e9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FromDigits","category":"page"},{"location":"#FromDigits","page":"Home","title":"FromDigits","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FromDigits.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FromDigits]","category":"page"}]
+}
